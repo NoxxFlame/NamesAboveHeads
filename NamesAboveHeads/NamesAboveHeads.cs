40a39d4f037940e78c7ae7562a74a203fb2ffb41
NoxxFlame <noxxflame@gmail.com>
2022-12-21 12:40:04 +0000

    Add project files.

---
+++ NamesAboveHeads/NamesAboveHeads.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using KitchenMods;
+
+namespace KitchenNamesAboveHeads
+{
+    internal class NamesAboveHeads : IModInitializer
+    {
+        public void PostActivate(Mod mod)
+        {
+            var harmony = new Harmony("noxxflame.plateup.namesaboveheads");
+            harmony.PatchAll(GetType().Assembly);
+        }
+
+        public void PreInject() { }
+
+        public void PostInject() { }
+    }
+}
---
+++ NamesAboveHeads/Patches/PlayerView_Patch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Kitchen;
+using TMPro;
+
+namespace KitchenNamesAboveHeads
+{
+    [HarmonyPatch(typeof(PlayerView))]
+    public class PlayerView_Patch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        public static bool Update_PrefixPatch(PlayerView __instance)
+        {
+            Traverse PV = Traverse.Create(__instance);
+            PV.Field("UseNameLabel").SetValue(true);
+            return true;
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        public static void Update_PostfixPatch(PlayerView __instance)
+        {
+            Traverse PV = Traverse.Create(__instance);
+            TextMeshPro nameLabel = (TextMeshPro)PV.Field("NameLabel").GetValue();
+            PlayerView.ViewData data = (PlayerView.ViewData)PV.Field("Data").GetValue();
+            nameLabel.text = Players.Main.Get(data.PlayerID).Name;
+            PV.Field("NameLabel").SetValue(nameLabel);
+        }
+    }
+}
